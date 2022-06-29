9122a98a6bae1d682e22597a30786739f27097e1
Andre0n <andreon098@gmail.com>
2022-06-29 13:28:18 -0300

    Include new funcitons

--- src/include/util.h
+++ src/include/util.h
@@ -8,15 +8,17 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+#include "globals.h"
+
 /* Procedure printToken prints a token
  * and its lexeme to the listing file
  */
-void printToken(TokenType, const char*);
+void printToken(TokenType token, const char* tokenString);
 
 /* Function newStmtNode creates a new statement
  * node for syntax tree construction
  */
-TreeNode* newStmtNode(StmtKind);
+TreeNode* newStmtNode(StmtKind kind);
 
 /* Function newExpNode creates a new expression
  * node for syntax tree construction
@@ -32,5 +34,7 @@ char* copyString(char*);
  * listing file using indentation to indicate subtrees
  */
 void printTree(TreeNode*);
+
+void freeTree(TreeNode* tree);
 
 #endif
